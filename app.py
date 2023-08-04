c1bde088a5cf791baf47e1af460d04d695be1bb9
Shyam Singh <2021pietcsshyam165@poornima.org>
2023-08-04 19:23:07 +0530

    changes in devision func

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 def Addition (a, v, c):
     return a+v+c
 
-<<<<<<< HEAD
 def Substraction(b, v, f):
     return b-v-f
-=======
-def Substraction(b, v, e):
-    return b-v-e
->>>>>>> ec93758974aa27665bcf4ef23d40a440a009e197
 
-def Devision (c, v):
-    return c/v
+def Devision (c, v, h):
+    return c/v/h
